9fada49327eca7984e8675704a77c23894e38a4e
Aboud <aboudsabry19@gmail.com>
2023-08-05 12:22:02 +0300

    first commit

---
+++ javascript.js
@@ -0,0 +1,8 @@
+
+// javascript function that calculates age based on user input
+function change() {
+    var year1 = document.getElementById('year1').value
+    var age = 2023-year1
+    return document.getElementById('para').innerHTML = age
+
+}
